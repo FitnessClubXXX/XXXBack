47d17b0c9e55e47409e89c67ba80f2f8be12b9ee
Jarek Danielak <danielak.jarek@gmail.com>
2020-01-19 15:18:59 +0100

    Init mongodb

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import User from './schemas/user';
 
 const app = express();
 const port = 3003;
+const connectionString = 'mongodb://heroku_m40drlsk:k14eq2dh8f0u00dkvm5o6ervd8@ds141631.mlab.com:41631/heroku_m40drlsk';
 
-app.get('/', (req, res) => {
-    return res.send('REST API is working fine!');
+// Establish connection to database
+mongoose.connect(connectionString, { useNewUrlParser: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', function () {
+  console.log("Connection successful!");
+});
+
+// User collection endpoints
+app.get('/users', (req, res) => {
+  User.find().then((doc) => {
+    res.send('index', {items: doc});
+  })
 });
 
 app.listen(port, () => {
-    console.log(`App is listening on port ${port}`);
+  console.log(`App is listening on port ${port}`);
 });
